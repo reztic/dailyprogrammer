56d426f4808624304e415747b407c7749c4a42c5
Chris <chriswong096@gmail.com>
2016-02-27 09:18:54 -0800

    no message

--- 255/main.cpp
+++ 255/main.cpp
@@ -2,30 +2,127 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
-
+#include <fstream>
+#include <map>
 
 using namespace std;
+
+
+string cleanString(string input)
+{
+    string output = "";
+    for ( string::iterator it = input.begin(); it != input.end(); it ++)
+    {
+        if (isalpha(*it))
+        {
+            output += *it;
+        }
+    }
+    return output;
+}
 
 vector<string> tokenizeString(string input)
 {
     vector<string> tString;
-    int index = 0;
-    while ( input.find("\n", index) == string::npos)
+    string temp = "";
+    for ( string::iterator it = input.begin(); it != input.end(); it ++)
     {
-        size_t end = input.find("\n", index);
-        tString.push_back( input.substr(index, end - index) );
-        index = static_cast<int>(end) + 1;
+        if ( *it == '\n' || it+1 == input.end() )
+        {
+            tString.push_back(temp);
+            temp = "";
+        }
+        else if (*it != ' ' && *it != '.')
+        {
+            temp += *it;
+        }
     }
-    tString.push_back( input.substr(index, input.length() - index) );
     return tString;
 }
 
+//Some weird method to do it.
+vector<string> methodOne(int groupingSize, vector<string> vString)
+{
+    //key = occurances, vector string = cause we could has mutliple string with same occurance.
+    map< int, vector<string> > everyBestCandidates;
+    
+    vector<string> output;
+    for( vector<string>::iterator it = vString.begin(); it != vString.end();++it)
+    {
+        string current = *it;
+        string largestFoundString = "";
+        int sizeOfLargestFoundSet = 0;
+        for( int x = 0; x < current.size(); x++)
+        {
+            int currentFoundSet = 0;
+            string groupString = current.substr(x, groupingSize);
+            if (groupString.size() == groupingSize)
+            {
+                //data[distance(vString.begin(),it)].push_back(groupString);
+                for( vector<string>::iterator rt = it; rt != vString.end();++rt)
+                {
+                    if ((*rt).find(groupString) != string::npos)
+                    {
+                        currentFoundSet++;
+                    }
+                }
+                if (currentFoundSet > sizeOfLargestFoundSet)
+                {
+                    largestFoundString = groupString;
+                    sizeOfLargestFoundSet = currentFoundSet;
+                }
+            }
+        }
+        everyBestCandidates[sizeOfLargestFoundSet].push_back(largestFoundString);
+    }
+    
+    int counter = 0;
+    for ( map< int, vector<string> >::reverse_iterator it = everyBestCandidates.rbegin(); it != everyBestCandidates.rend(); ++it)
+    {
+        for( vector<string>::iterator vt = (*it).second.begin(); vt != (*it).second.end(); ++vt)
+        {
+            counter ++;
+            int occurances = 0;
+            for( vector<string>::iterator rt = vString.begin(); rt != vString.end();)
+            {
+                if ((*rt).find((*vt)) != string::npos)
+                {
+                    occurances++;
+                    rt = vString.erase(rt);
+                }
+                else
+                {
+                    ++rt;
+                }
+            }
+            if (occurances > 0)
+                output.push_back(*vt);
+        }
+        if ( vString.size() == 0) //Quick escape
+            break;
+    }
+    return output;
+}
+
+
 int main()
 {
-    string input ="Jack and Jill went up the hill to fetch a pail of water.\nAll work and no play makes Jack and Jill a dull couple.\nThe Manchester United Junior Athletic Club (MUJAC) karate team was super good at kicking.\nThe MUJAC playmaker actually kinda sucked at karate.";
+    int groupingSize = 5;
     
-    vector<string> tString = tokenizeString(input);
-	
+    //string input ="Jack and Jill went up the hill to fetch a pail of water.\nAll work and no play makes Jack and Jill a dull couple.\nThe Manchester United Junior Athletic Club (MUJAC) karate team was super good at kicking.\nThe MUJAC playmaker actually kinda sucked at karate.";
+    //vector<string> vString = tokenizeString(input);
+    
+    vector<string> vString;
+    ifstream file("oneliners.txt");
+    string str;
+    while (std::getline(file, str))
+    {
+        vString.push_back(cleanString(str));
+    }
+    
+    vector<string> output = methodOne(groupingSize, vString);
+    
+    cout << output.size() << endl;
     return 0;
 }
 
